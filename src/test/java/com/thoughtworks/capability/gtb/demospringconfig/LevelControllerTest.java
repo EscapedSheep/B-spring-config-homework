9419d2f69aeb6e9b824a9a7436c7b25e98d5b32d
EscapedSheep <sfgmdf001@126.com>
2020-09-18 23:32:48 +0800

    [maida] completed p2

--- src/test/java/com/thoughtworks/capability/gtb/demospringconfig/LevelControllerTest.java
+++ src/test/java/com/thoughtworks/capability/gtb/demospringconfig/LevelControllerTest.java
@@ -1,12 +1,17 @@
 package com.thoughtworks.capability.gtb.demospringconfig;
 
+import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.util.ReflectionTestUtils;
 import org.springframework.test.web.servlet.MockMvc;
 
 import static org.hamcrest.Matchers.*;
 import static org.junit.jupiter.api.Assertions.*;
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.jsonPath;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
 
 @SpringBootTest
 @AutoConfigureMockMvc
@@ -14,4 +19,19 @@ class LevelControllerTest {
     @Autowired
     private MockMvc mockMvc;
 
+    @Autowired
+    private LevelController levelController;
+
+    @Test
+    void should_receive_advanced_when_level_number_is_greater_than_1() throws Exception {
+        mockMvc.perform(get("/level"))
+                .andExpect(status().isOk())
+                .andExpect(jsonPath("$", is("basic")));
+
+        ReflectionTestUtils.setField(levelController, "levelNumber", 2);
+        mockMvc.perform(get("/level"))
+                .andExpect(status().isOk())
+                .andExpect(jsonPath("$", is("advanced")));
+
+    }
 }
